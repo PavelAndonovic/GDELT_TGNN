a6283143907860f995ac08e9948571ed420f30e6
PavelAndonovic <andonovicpavel@gmail.com>
2022-04-29 23:04:04 +0200

    Added data transformation notebooks, and methods for pulling yearly gdelt data

--- data_processing/data_pull.py
+++ data_processing/data_pull.py
@@ -6,14 +6,14 @@ if __name__ == '__main__':
     gd2 = gdelt.gdelt(version=1)
 
     # Single 15 minute interval pull, output to json format with mentions table
-    #for year in range(1979, 2015):
-    for year in range(2009, 2015):
+    # for year in range(1979, 2015):
+    for year in range(1979, 2015):
         start = datetime.datetime.now()
         base = datetime.datetime(year, 1, 1)
-        date_list = [base + datetime.timedelta(days=x) for x in range(365)]
-        if datetime.datetime(year+1, 1, 1) in date_list:
+        date_list = [base + datetime.timedelta(days=x) for x in range(356)]
+        if datetime.datetime(year + 1, 1, 1) in date_list:
             date_list.pop(-1)
-        #for date in date_list:
+        # for date in date_list:
         date_list_ = [" ".join(str(date).split(" ")[0].split("-")) for date in date_list]
         results = gd2.Search(date_list_, table='events', output='csv')
         with open("..\\data\\temp_pull.csv", "w", encoding="utf-8") as f:
@@ -25,18 +25,19 @@ if __name__ == '__main__':
         temp.dropna(subset=['Actor1CountryCode'], how='all', inplace=True)
         temp.dropna(subset=['Actor2CountryCode'], how='all', inplace=True)
         temp['NumberOfRowsBetweenCountries'] = 1
+        temp['Verbal Cooperation'] = temp['QuadClass'].apply(lambda x: 1 if x == 1 else 0)
+        temp['Material Cooperation'] = temp['QuadClass'].apply(lambda x: 1 if x == 2 else 0)
+        temp['Verbal Conflict'] = temp['QuadClass'].apply(lambda x: 1 if x == 3 else 0)
+        temp['Material Conflict'] = temp['QuadClass'].apply(lambda x: 1 if x == 4 else 0)
         temp['AlternateGoldsteinScale'] = temp.apply(lambda x: x.GoldsteinScale * x.NumArticles, axis=1)
         temp = temp.groupby(["Actor1CountryCode", "Actor2CountryCode"]).agg(
             {"Actor1CountryCode": "max", "Actor2CountryCode": "max", "AlternateGoldsteinScale": "sum",
-             "SQLDATE": "max", "QuadClass": "max", "GoldsteinScale": "mean", "NumArticles": "sum",
+             "SQLDATE": "max", 'Material Cooperation': "mean", 'Verbal Cooperation': "mean", 'Verbal Conflict': "mean",
+             'Material Conflict': "mean", "GoldsteinScale": "mean", "NumArticles": "sum",
              "AvgTone": "mean", "Actor1Geo_Lat": "mean", "Actor2Geo_Lat": "mean",
              "Actor1Geo_Long": "mean", "Actor2Geo_Long": "mean", 'NumberOfRowsBetweenCountries': "sum"})
         temp['AlternateGoldsteinScale'] = temp.apply(lambda x: x.AlternateGoldsteinScale / x.NumArticles, axis=1)
         temp.to_csv("..\\data\\temp_pull.csv", encoding="utf-8", index=False)
 
         temp.to_csv(f"..\\data\\gdelt_data\\{year}_gdelt.csv")
-        print(datetime.datetime.now()-start)
-
-
-
-
+        print(datetime.datetime.now() - start)
